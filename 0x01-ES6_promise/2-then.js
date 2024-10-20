550e73b7409c90c0dbebcdb43c5c7f3a11a8e693
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-10-21 00:54:24 +0300

    Adding new file

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then((resolve) => {
-    console.log('Got a response from the API');
-    return {
-      status: 200,
-      body: 'Success',
-    };
-  }).catch((e) => {
-    return e;
-  });
+  return promise
+    .then(() => ({status: 200, body: 'success'}))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }

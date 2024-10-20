6b69cf55c8b90f3c201735e3092f745021bc9b8f
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-10-21 01:46:59 +0300

    Adding new file

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,11 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstname, lastName, fileName) {
   return Promise
-  .allSettled([signUpUser(firstname, lastName), uploadPhoto(fileName)])
+    .allSettled([signUpUser(firstname, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }

e5bc7f197c6d6d8914e814b69f1b8500c3ac8059
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2024-10-21 00:43:16 +0300

    Adding new file

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
 export default async function getFullResponseFromAPI(success) {
- if (success) {
-  return {
-    status: 200,
-    body: 'Success',
-  };
- } else {
+  if (success) {
+    return {
+      status: 200,
+      body: 'Success',
+    };
+  }
   throw new Error('The fake API is not working currently');
- }
 }
